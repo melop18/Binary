33db65250c3e8e87a1f707a5df0febef5da690cb
melop18 <matepajaromagico@gmail.com>
2025-03-19 19:56:47 +0000

    Modified

---
+++ Binary/Binary/__init__.py
@@ -0,0 +1,3 @@
+import Binary.get, Binary.transform
+from .transform import main
+from .func import hola
---
+++ Binary/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Binary',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Melop',
+    author_email='melop18076@gmail.com',
+    description ='A library for binary operations.',
+)
